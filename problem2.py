672eaa0b7bfde05ef7722fe33ceeaab4bc7a3454
Divyanshi Srivastava <divyanshisrivastava@Mac.attlocal.net>
2025-02-15 19:54:30 -0800

    python

--- problem2.py
+++ problem2.py
@@ -6,3 +6,24 @@
 
 // Your code here along with comments explaining your approach
 '''
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        idx = 0
+        inorderMap = {}
+        for i in range(len(inorder)):
+            inorderMap[inorder[i]] = i
+
+        def helper(start, end):
+            # base
+            if start > end: return None
+            nonlocal preorder, idx, inorderMap
+            rootVal = preorder[idx]
+            idx += 1
+            root = TreeNode(rootVal)
+            rootIdx = inorderMap[rootVal]
+            
+            root.left = helper(start, rootIdx -1)
+            root.right = helper(rootIdx + 1, end)
+            return root
+
+        return helper(0, len(inorder) - 1)
